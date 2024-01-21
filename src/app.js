a7abb53daae1829e1c893d360b8310d6e58a9628
Uriel Agüero <uriel.aguero1812@gmail.com>
2024-01-21 18:16:45 -0300

    Prueba de cors 7

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
-app.use(cors({origin: 'http://127.0.0.1:5500'}));
+app.use(cors({origin: '*'}));
 app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
