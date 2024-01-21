ba52195a6af15a8dea15d97308787f61beb7dcf3
Uriel Agüero <uriel.aguero1812@gmail.com>
2024-01-21 18:13:46 -0300

    Prueba de cors 6

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ import CustomCheckoutRouter from './dao/MongoDb/routes/customCheckout.router.js'
 const app = express();
 // app.use(cors({origin: allowedFetchOrigins}));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
