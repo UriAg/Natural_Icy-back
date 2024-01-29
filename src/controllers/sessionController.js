d6389277f7d01de18e40ef544281c4a44e289b27
Uriel Agüero <uriel.aguero1812@gmail.com>
2024-01-29 00:07:22 -0300

    modificacion de codigos de status

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -151,7 +151,6 @@ async function generateLogin(req, res, next){
                 code: errorTypes.AUTHORIZATION_ERROR
             })
         }
-        console.log(req.user)
         let token = generateJWT(req.user)
 
         return res.status(200).json({payload:`Logueado correctamente`, generateUserTokenAccess: token, userRole:req.user.role})
--- src/middlewares/errors/handleErrors.js
+++ src/middlewares/errors/handleErrors.js
@@ -2,22 +2,22 @@ import { errorTypes } from "../../services/errors/enums.js";
 export default (error, req, res, next)=>{
     switch(error.code){
         case errorTypes.AUTHENTICATION_ERROR:
-            res.json({status:'Authentication error', error:error.name, description:error.description, code: error.code})
+            res.status(AUTHENTICATION_ERROR).json({status:'Authentication error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.AUTHORIZATION_ERROR:
-            res.json({status:'Authorization error', error:error.name, description:error.description, code: error.code})
+            res.status(AUTHORIZATION_ERROR).json({status:'Authorization error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.INVALID_ARGS_ERROR:
-            res.json({status:'Bad request error', error:error.name, description:error.description, code: error.code})
+            res.status(INVALID_ARGS_ERROR).json({status:'Bad request error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.NOT_FOUND_ERROR:
-            res.json({status:'Not found error', error:error.name, description:error.description, code: error.code})
+            res.status(NOT_FOUND_ERROR).json({status:'Not found error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.SERVER_SIDE_ERROR:
-            res.json({status:'Server side error', error:error.name, description:error.description, code: error.code})
+            res.status(SERVER_SIDE_ERROR).json({status:'Server side error', error:error.name, description:error.description, code: error.code})
             break;
         default: 
-            res.json({status:'Error', error:'Unhandled error'})
+            res.status(500).json({status:'Error', error:'Unhandled error'})
     }
   
 }
