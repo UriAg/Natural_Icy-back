4009a65c365b0eca6cdf0894695297da15391653
Uriel Agüero <uriel.aguero1812@gmail.com>
2024-01-29 11:12:13 -0300

    modificacion de codigos de status 3

--- src/middlewares/errors/handleErrors.js
+++ src/middlewares/errors/handleErrors.js
@@ -2,19 +2,19 @@ import { errorTypes } from "../../services/errors/enums.js";
 export default (error, req, res, next)=>{
     switch(error.code){
         case errorTypes.AUTHENTICATION_ERROR:
-            res.status(error.code).json({status:'Authentication error', error:error.name, description:error.description, code: error.code})
+            res.json({status:'Authentication error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.AUTHORIZATION_ERROR:
-            res.status(error.code).json({status:'Authorization error', error:error.name, description:error.description, code: error.code})
+            res.json({status:'Authorization error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.INVALID_ARGS_ERROR:
-            res.status(error.code).json({status:'Bad request error', error:error.name, description:error.description, code: error.code})
+            res.json({status:'Bad request error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.NOT_FOUND_ERROR:
-            res.status(error.code).json({status:'Not found error', error:error.name, description:error.description, code: error.code})
+            res.json({status:'Not found error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.SERVER_SIDE_ERROR:
-            res.status(error.code).json({status:'Server side error', error:error.name, description:error.description, code: error.code})
+            res.json({status:'Server side error', error:error.name, description:error.description, code: error.code})
             break;
         default: 
             res.status(500).json({status:'Error', error:'Unhandled error'})
