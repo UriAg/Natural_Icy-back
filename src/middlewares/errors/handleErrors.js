08b84de911b428635f2a122dae86e69b8a899cc4
Uriel Agüero <uriel.aguero1812@gmail.com>
2024-01-29 00:16:36 -0300

    modificacion de codigos de status 2

--- src/middlewares/errors/handleErrors.js
+++ src/middlewares/errors/handleErrors.js
@@ -2,19 +2,19 @@ import { errorTypes } from "../../services/errors/enums.js";
 export default (error, req, res, next)=>{
     switch(error.code){
         case errorTypes.AUTHENTICATION_ERROR:
-            res.status(AUTHENTICATION_ERROR).json({status:'Authentication error', error:error.name, description:error.description, code: error.code})
+            res.status(error.code).json({status:'Authentication error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.AUTHORIZATION_ERROR:
-            res.status(AUTHORIZATION_ERROR).json({status:'Authorization error', error:error.name, description:error.description, code: error.code})
+            res.status(error.code).json({status:'Authorization error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.INVALID_ARGS_ERROR:
-            res.status(INVALID_ARGS_ERROR).json({status:'Bad request error', error:error.name, description:error.description, code: error.code})
+            res.status(error.code).json({status:'Bad request error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.NOT_FOUND_ERROR:
-            res.status(NOT_FOUND_ERROR).json({status:'Not found error', error:error.name, description:error.description, code: error.code})
+            res.status(error.code).json({status:'Not found error', error:error.name, description:error.description, code: error.code})
             break;
         case errorTypes.SERVER_SIDE_ERROR:
-            res.status(SERVER_SIDE_ERROR).json({status:'Server side error', error:error.name, description:error.description, code: error.code})
+            res.status(error.code).json({status:'Server side error', error:error.name, description:error.description, code: error.code})
             break;
         default: 
             res.status(500).json({status:'Error', error:'Unhandled error'})
