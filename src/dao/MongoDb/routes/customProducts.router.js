5b9880c63b28612e99acb7b1a09d14087bf506aa
Uriel Agüero <uriel.aguero1812@gmail.com>
2024-02-18 23:03:45 -0300

    Reparación de multer 6

--- src/dao/MongoDb/routes/customProducts.router.js
+++ src/dao/MongoDb/routes/customProducts.router.js
@@ -18,7 +18,7 @@ class CustomProductsRouter extends MyRouter{
 
         this.post('/', ['ADMIN', 'CREATOR'], passportCall('jwt'), uploadImage.array('thumbnail', 5), productController.uploadProductToDB)
 
-        this.put('/:productId', ['ADMIN', 'CREATOR'], passportCall('jwt'), (req, res, next)=>{console.log('empieza')},uploadImage.array('thumbnail', 5), (req, res, next)=>{console.log('TERMINa')} , productController.editProductFromDB)
+        this.put('/:productId', ['ADMIN', 'CREATOR'], passportCall('jwt'), (req, res, next)=>{console.log('empieza'), next()},uploadImage.array('thumbnail', 5), (req, res, next)=>{console.log('TERMINa'); next()} , productController.editProductFromDB)
         
         // this.put('/:productId/images', ['ADMIN', 'CREATOR'], passportCall('jwt'), uploadImage.array('thumbnail', 5), productController.addProductImagesFromDB)
         
